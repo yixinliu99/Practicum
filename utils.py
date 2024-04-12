1433d51b8764f654990319631a04a26d4763da91
ihsinliu <ihsin.liu@outlook.com>
2024-04-12 01:37:48 -0500

    1

--- utils.py
+++ utils.py
@@ -5,12 +5,6 @@ DEEP_ARCHIVE = 'DEEP_ARCHIVE'
 INTELLIGENT_TIERING = 'INTELLIGENT_TIERING'
 ARCHIVE_CLASSES = [GLACIER, DEEP_ARCHIVE, INTELLIGENT_TIERING]
 
-def get_objects(bucket_name):
-    s3 = boto3.client('s3')
-    response = s3.list_objects_v2(Bucket=bucket_name)
-
-    return response
-
 
 def thaw_objects(complete_path, action_id):
     s3 = boto3.client('s3')
@@ -20,9 +14,9 @@ def thaw_objects(complete_path, action_id):
     print(complete_path)
     print(source_bucket)
     print(name)
-    response = s3.list_objects_v2(Bucket=source_bucket) # todo: pagination (1k items)
-    print(response)
-    for obj in response['Contents']:
+    list_objects = s3.list_objects_v2(Bucket=source_bucket)  # todo: list_objects_v2 pagination (1k items max/ page)
+    print(list_objects)
+    for obj in list_objects['Contents']:
         if not obj['Key'].startswith(name):
             continue
 
@@ -32,28 +26,13 @@ def thaw_objects(complete_path, action_id):
                 s3.restore_object(Bucket=source_bucket, Key=obj['Key'], RestoreRequest={'Days': 1})
                 dynamodb.put_item(TableName='MPCS-Practicum-2024',
                                   Item={'action_id': {'S': action_id}, 'object_id': {'S': obj['Key']}})
+                set_notification(obj['Key'], action_id)
             except Exception as e:
                 print(e)
-                return False  # todo
+                return False  # todo: error handling
 
     return True
 
-
-def set_bucket_object_thawed_notification(bucket_name, lambda_arn):
-    s3 = boto3.client('s3')
-    s3.put_bucket_notification_configuration(
-        Bucket=bucket_name,
-        NotificationConfiguration={
-            'LambdaFunctionConfigurations': [
-                {
-                    'LambdaFunctionArn': lambda_arn,
-                    'Events': [
-                        's3:ObjectRestore:Completed'
-                    ]
-                },
-            ]
-        }
-    )
 
 if __name__ == "__main__":
     print(thaw_objects('mpcs-practicum/testdata', '1'))
