3c6463c6a39c156fcd62a3826f81127bbedf76f9
yixinliu <ihsin.liu@outlook.com>
2024-05-08 02:03:19 +0000

    get RestoreStatus as extra param when get objects

--- model/ThawMetadata.py
+++ model/ThawMetadata.py
@@ -1,10 +1,10 @@
 from dataclasses import dataclass
 from model.ThawStatus import ThawStatus
+import json
 
 
 @dataclass
 class ThawMetadata:
-
     action_id: str
     object_id: str
     status: ThawStatus
@@ -13,15 +13,22 @@ class ThawMetadata:
     expiration_time: str
 
     def __init__(self, action_id, object_id, status: ThawStatus, start_time, completion_time, expiration_time):
+        if not action_id:
+            raise ValueError('action_id is required')
+        if not object_id:
+            raise ValueError('object_id is required')
+        if not status:
+            raise ValueError('status is required')
+
         self.action_id = action_id
         self.object_id = object_id
         self.status = status
-        self.start_time = start_time
-        self.completion_time = completion_time
-        self.expiration_time = expiration_time
+        self.start_time = start_time if start_time else ''
+        self.completion_time = completion_time if completion_time else ''
+        self.expiration_time = expiration_time if expiration_time else ''
 
     def marshal(self):
-        return {
+        res = {
             'action_id': {'S': self.action_id},
             'object_id': {'S': self.object_id},
             'status': {'S': self.status},
@@ -29,6 +36,8 @@ class ThawMetadata:
             'completion_time': {'S': self.completion_time},
             'expiration_time': {'S': self.expiration_time}
         }
+
+        return res
 
     @staticmethod
     def parse(item):
--- utils.py
+++ utils.py
@@ -2,7 +2,8 @@ import concurrent
 import boto3
 from model.ThawMetadata import ThawMetadata
 from model.ThawStatus import ThawStatus
-import datetime
+from datetime import *
+from dateutil.tz import *
 
 GLACIER = 'GLACIER'
 DEEP_ARCHIVE = 'DEEP_ARCHIVE'
@@ -21,24 +22,34 @@ def thaw_objects(complete_path, action_id):
     print(name)
 
     paginator = s3.get_paginator('list_objects_v2')
-    pages = paginator.paginate(Bucket=source_bucket)
+    pages = paginator.paginate(
+        Bucket=source_bucket,
+        Prefix=name,
+        OptionalObjectAttributes=
+        [
+            'RestoreStatus',
+        ]
+    )
+
     for page in pages:
         for obj in page['Contents']:
-            if not obj['Key'].startswith(name):
-                continue
-
-            obj_class = obj['StorageClass']
-            if obj_class in ARCHIVE_CLASSES:
+            if obj['StorageClass'] in ARCHIVE_CLASSES:
                 keys.append(obj['Key'])
+                status = ThawStatus.INITIATED
+                if 'RestoreStatus' in obj and not obj['RestoreStatus']['IsRestoreInProgress'] and datetime.now(tzlocal()) < obj['RestoreStatus']['RestoreExpiryDate']:
+                    status = ThawStatus.COMPLETED
 
-    metadata = ThawMetadata(action_id,
-                            complete_path,
-                            ThawStatus.INITIATED,
-                            datetime.datetime.now().isoformat(),
-                            None,
-                            None)
+                metadata = ThawMetadata(action_id,
+                                        source_bucket + "/" + obj['Key'],
+                                        status,
+                                        datetime.now().isoformat(),
+                                        None,
+                                        None)
+
+                put_thaw_metadata(metadata, dynamodb)
+
+    set_s3_notification(source_bucket, keys, s3)
     init_s3_restore(source_bucket, keys, s3)
-    put_thaw_metadata(metadata, dynamodb)
 
     return True
 
@@ -59,9 +70,44 @@ def init_s3_restore(source_bucket: str, keys: list, s3_client: boto3.client):
                 return False
 
 
+def set_s3_notification(bucket_name: str, keys: [str], s3_client: boto3.client):
+    filter_rules = []
+    for k in keys:
+        filter_rules.append({
+            'Name': 'prefix',
+            'Value': k,
+        })
+
+    notification_configuration = {
+        'TopicConfigurations': [
+            {
+                'TopicArn': 'arn:aws:sns:us-east-1:074950442422:Practicum-2024',  # todo: SNS ARN
+                'Events': [
+                    's3:ObjectRestore:*'
+                ],
+                'Filter': {
+                    "Key": {
+                        'FilterRules': filter_rules
+                    }
+                }
+            }
+        ]
+    }
+    try:
+        response = s3_client.put_bucket_notification_configuration(
+            Bucket=bucket_name,
+            NotificationConfiguration=notification_configuration
+        )
+        print(response)
+
+    except Exception as e:
+        print(e)
+
+
 def put_thaw_metadata(metadata: ThawMetadata, dynamodb_client: boto3.client):
-    dynamodb_client.put_item(TableName='MPCS-Practicum-2024', Item=metadata.marshal()) # todo: table name
+    dynamodb_client.put_item(TableName='MPCS-Practicum-2024', Item=metadata.marshal())  # todo: table name
 
 
 if __name__ == "__main__":
-    print(thaw_objects('mpcs-practicum/test00', '1'))
+    res = thaw_objects('mpcs-practicum/test00', '1')
+    print(res)
