f4ecc9698395ed21a585cd22791c65ea2a4be4cf
yixinliu <ihsin.liu@outlook.com>
2024-05-20 22:18:58 +0000

    feature: cleanup s3 notification config

--- DataTypes.py
+++ DataTypes.py
@@ -9,6 +9,7 @@ class DataTypes:
         self.OBJECT_ID = 'object_id'
         self.START_TIME = 'start_time'
         self.EXPIRY_TIME = 'expiry_time'
+        self.BUCKET_NAME = 'bucket_name'
         self.GLACIER = 'GLACIER'
         self.DEEP_ARCHIVE = 'DEEP_ARCHIVE'
         self.INTELLIGENT_TIERING = 'INTELLIGENT_TIERING'
@@ -17,6 +18,7 @@ class DataTypes:
         self.ACTION_STATUS_TABLE_NAME = params['ACTION_STATUS_TABLE_NAME']
         self.REGION_NAME = params['REGION_NAME']
         self.SNS_TOPIC_ARN = params['SNS_TOPIC_ARN']
-        self.GSI_INDEX_NAME = params['GSI_INDEX_NAME']
+        self.ACTION_STATUS_GSI_INDEX_NAME = params['ACTION_STATUS_GSI_INDEX_NAME']
+        self.BUCKET_GSI_INDEX_NAME = params['BUCKET_GSI_INDEX_NAME']
         self.S3_QUERY_INTERVAL = int(params['S3_QUERY_INTERVAL'])
 
