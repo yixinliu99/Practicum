4f6fb3d387956c6413b635ae8ab5b43be0de4733
yixinliu <ihsin.liu@outlook.com>
2024-05-29 13:00:05 -0500

    .env to json

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
 import logging
 from flask import Flask
-import config
 from thaw_action.profile import thaw_aptb
 from globus_action_provider_tools.flask.helpers import assign_json_provider
 from DataTypes import DataTypes
+import json
 
 
 def create_app():
     app = Flask(__name__)
+    app.datatypes = set_env()
+    app.config.from_object(app.datatypes)
     assign_json_provider(app)
     app.logger.setLevel(logging.DEBUG)
-    app.config.from_object(config)
     app.register_blueprint(thaw_aptb)
-    app.datatypes = set_env()
 
     return app
 
 
 def set_env() -> DataTypes:
-    vs = {}
     with open("./.env", 'r') as file:
-        for line in file:
-            key, value = line.strip().split(' = ')
-            vs[key] = value
+        vs = json.load(file)
     dt = DataTypes(vs)
-
     return dt
 
 
--- config.py
+++
@@ -1,4 +0,0 @@
-from flask import current_app
-
-APT_CLIENT_ID = current_app.datatypes.APT_CLIENT_ID
-APT_CLIENT_SECRET = current_app.datatypes.APT_CLIENT_SECRET
